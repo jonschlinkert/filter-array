a871b3e7d551e3340febb96e79065ddc5c5ce78c
tunnckoCore <mameto_100@mail.bg>
2015-03-18 20:32:35 +0200

    refactor + docs

--- index.js
+++ index.js
@@ -1,31 +1,56 @@
 /*!
  * filter-array <https://github.com/jonschlinkert/filter-array>
  *
- * Copyright (c) 2014 Jon Schlinkert, contributors.
- * Licensed under the MIT license.
+ * Copyright (c) 2014-2015 Jon Schlinkert, contributors.
+ * Licensed under the MIT License
  */
 
 'use strict';
 
-var makeIterator = require('make-iterator');
+var typeOf = require('kind-of');
+var filter = require('arr-filter');
+var mm = require('micromatch');
 
-module.exports = function filter(arr, cb, thisArg) {
-  cb = makeIterator(cb, thisArg);
-  if (arr == null) {
+/**
+ * Filter array against given glob
+ * patterns, regex or given function.
+ *
+ * For more examples see the [tests](./test.js)
+ *
+ * **Example:**
+ * ```js
+ * var filter = require('filter-array');
+ *
+ * filter(['a', 'b', 'c', 'b', 'c', 'e'], function(ele) {
+ *   return ele === 'a' || ele === 'b';
+ * });
+ *
+ * //=> ['a', 'b', 'b']
+ * ```
+ *
+ * @name   filterArray
+ * @param  {Array} `arr` array to filter
+ * @param  {Array|String|Function|RegExp} `filters`
+ * @param  {Object} `opts` options to pass to [micromatch]
+ * @return {Array}
+ * @api public
+ */
+module.exports = function filterArray(arr, filters, opts) {
+  if (arr.length === 0) {
     return [];
   }
 
-  var len = arr.length;
-  var i = 0;
-  var res = [];
-  var val;
+  if (typeOf(filters) === 'function' || typeOf(filters) === 'regexp') {
+    var isMatch = mm.matcher(filters, opts);
 
-  while (len--) {
-    val = arr[i++];
-    if (cb(val, i, arr)) {
-      res.push(val);
-    }
+    return filter(arr, function _filter(val) {
+      return isMatch(val);
+    });
   }
 
-  return res;
+  if (typeOf(filters) === 'string' || typeOf(filters) === 'array') {
+    return filter(arr, mm.filter(filters, opts));
+  }
+
+  return [];
 };
--- test.js
+++ test.js
@@ -1,21 +1,58 @@
-/*!
- * array-filter <https://github.com/jonschlinkert/array-filter>
+/**
+ * filter-array <https://github.com/jonschlinkert/filter-array>
  *
- * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Copyright (c) 2014-2015 Jon Schlinkert, contributors.
  * Licensed under the MIT License
  */
 
 'use strict';
 
-var should = require('should');
-var filter = require('./');
+var assert = require('assert');
+var filter = require('./index');
 
-describe('filter', function () {
-  it('should filter an array:', function () {
-    var res = filter(['a', 'b', 'c', 'b', 'c', 'e'], function (ele) {
+describe('filter', function() {
+  it('should return empty array if empty array is given', function(done) {
+    var actual = filter([]);
+    var expected = [];
+
+    assert.deepEqual(actual, expected);
+    done();
+  });
+  it('should return empty array no function, string, regex or array given', function(done) {
+    var actual = filter({one: 'two'});
+    var expected = [];
+
+    assert.deepEqual(actual, expected);
+    done();
+  });
+  it('should filter an array when function given', function(done) {
+    var actual = filter(['a', 'b', 'c', 'b', 'c', 'e'], function(ele) {
       return ele === 'a' || ele === 'b';
     });
+    var expected = ['a', 'b', 'b'];
 
-    res.should.eql(['a', 'b', 'b']);
+    assert.deepEqual(actual, expected);
+    done();
+  });
+  it('should filter an array with regex', function(done) {
+    var actual = filter(['a', 'b', 'c', 'b', 'b', 'f'], /(a|b)/);
+    var expected = ['a', 'b', 'b', 'b'];
+
+    assert.deepEqual(actual, expected);
+    done();
+  });
+  it('should filter an array when glob string given', function(done) {
+    var actual = filter(['a', 'b', 'c', 'b', 'c', 'e'], '{a,b}');
+    var expected = ['a', 'b', 'b'];
+
+    assert.deepEqual(actual, expected);
+    done();
+  });
+  it('should filter an array when array of glob patterns given', function(done) {
+    var actual = filter(['a', 'b', 'c', 'd', 'e', 'f', 'g'], ['{a..g}', '![b-e]']);
+    var expected = ['a', 'f', 'g'];
+
+    assert.deepEqual(actual, expected);
+    done();
   });
 });
